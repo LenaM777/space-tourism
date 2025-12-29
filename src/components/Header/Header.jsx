822644fb63ec7da4241b410450c680015b3c987e
Olena Mustafina <toysamiy.mail@gmail.com>
2025-12-29 18:09:24 +0200

    feat: Add all pages to router and add Header component to Layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,12 @@
 import "./Header.scss";
 
-const Header = () => {
-  return <div>Header</div>;
-};
-
-export default Header;
+export default function Header() {
+  return (
+    <div>
+      <a href="/">home</a>
+      <a href="/destination">destination</a>
+      <a href="/crew">crew</a>
+      <a href="/technology">technology</a>
+    </div>
+  );
+}
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import "../styles/global.scss";
+import Header from "../components/Header/Header";
 
 export default function Layout() {
   return (
     <div className="app-wrapper">
       <div className="container">
-        <div>header</div>
+        <Header />
         <main>
           <Outlet />
         </main>
