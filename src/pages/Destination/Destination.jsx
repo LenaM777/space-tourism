ac94a94b6bf470b6ed188e6ec837bfcc3b72ac9f
Olena Mustafina <toysamiy.mail@gmail.com>
2026-01-05 23:37:46 +0200

    feat: Add Destination page with base structure, backgrounds and styles, create page-title and paginations components, update mixins

--- src/pages/Destination/Destination.jsx
+++ src/pages/Destination/Destination.jsx
@@ -1,7 +1,37 @@
 import "./Destination.scss";
 
-const Destination = () => {
-  return <div>Destination</div>;
-};
-
-export default Destination;
+export default function Destination() {
+  return (
+    <section className="destination section">
+      <div className="section__container">
+        <h3 className="page-title">
+          <span>01</span>PICK YOUR DESTINATION
+        </h3>
+        <div className="destination__content">
+          <div className="destination__img"></div>
+          <div className="destination__info">
+            <div className="destination__tabs">
+              <button>name</button>
+              <button>name</button>
+            </div>
+            <div className="destination__exposition">
+              <h1 className="section-title">name</h1>
+              <p className="section-text">destination</p>
+            </div>
+            <hr className="destination__line"></hr>
+            <div className="destination__facts">
+              <div className="destination__fact">
+                <h6 className="destination__stat">AVG. DISTANCE</h6>
+                <p className="destination__values">distance</p>
+              </div>
+              <div className="destination__fact">
+                <h6 className="destination__stat">Est. travel time</h6>
+                <p className="destination__values">travel</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
