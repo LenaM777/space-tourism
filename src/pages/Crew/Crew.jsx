597790e40a76db6828907c7e449a165936c14d74
Olena Mustafina <toysamiy.mail@gmail.com>
2026-02-20 08:35:00 +0200

    feat: Add Crew page with html structure, images and styles, includes using map from data.json

--- src/pages/Crew/Crew.jsx
+++ src/pages/Crew/Crew.jsx
@@ -1,7 +1,43 @@
+import { useState } from "react";
+import data from "../../data.json";
 import "./Crew.scss";
 
-const Crew = () => {
-  return <div>Crew</div>;
-};
+export default function Crew() {
+  const [index, setIndex] = useState(0);
+  const { crew } = data;
+  const { name, role, bio, images } = crew[index];
 
-export default Crew;
+  return (
+    <section className=" crew">
+      <div className="section__container crew__container">
+        <h3 className="page-title">
+          <span>02</span>MEET YOUR CREW
+        </h3>
+        <div className="crew__content">
+          <div className="crew__info">
+            <div className="crew__exposition">
+              <div className="crew__name">
+                <h2 className="section-subtitle">{role}</h2>
+                <h1 className="section-title">{name}</h1>
+              </div>
+              <p className="crew__text section-text">{bio}</p>
+            </div>
+
+            <div className="crew__tabs">
+              {data.crew.map((person, i) => (
+                <button
+                  key={person.name}
+                  className={i === index ? "active" : ""}
+                  onClick={() => setIndex(i)}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="crew__image">
+            <img src={images.png} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
