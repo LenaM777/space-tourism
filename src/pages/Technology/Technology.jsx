6344267170839248f8f1f805cb9e412afbe8023d
Olena Mustafina <toysamiy.mail@gmail.com>
2026-02-27 17:07:30 +0200

    feat: Add motion and AnimatePresence for animations and positioning section for Technology page

--- src/pages/Technology/Technology.jsx
+++ src/pages/Technology/Technology.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import data from "../../data.json";
 import "./Technology.scss";
 
@@ -8,18 +9,29 @@ export default function Technology() {
   const { name, description, images } = technology[index];
 
   return (
-    <section className="technology section">
+    <section
+      className={`technology section ${
+        index === 2 ? "technology--left" : "technology--right"
+      }`}
+    >
       <div className="technology__container">
         <h3 className="page-title">
           <span>03</span>SPACE LAUNCH 101
         </h3>
         <div className="technology__content">
-          <div className="technology__image">
+          <motion.div
+            key={images.portrait}
+            className="technology__image "
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6 }}
+          >
             <picture>
               <source media="(min-width: 1024px)" srcSet={images.portrait} />
               <img src={images.landscape} alt={name} />
             </picture>
-          </div>
+          </motion.div>
+
           <div className="technology__info">
             <div className="technology__tabs">
               {data.technology.map((vehicle, i) => (
@@ -32,13 +44,22 @@ export default function Technology() {
                 </button>
               ))}
             </div>
-            <div className="technology__exposition">
-              <div className="technology__heading">
-                <h2 className="section-subtitle">THE TERMINOLOGY…</h2>
-                <h1 className="section-title">{name}</h1>
-              </div>
-              <p className="section-text">{description}</p>
-            </div>
+            <AnimatePresence mode="wait">
+              <motion.div
+                key={index}
+                initial={{ opacity: 0, x: 50 }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 0, x: -50 }}
+                transition={{ duration: 0.5 }}
+                className="technology__exposition"
+              >
+                <div className="technology__heading">
+                  <h2 className="section-subtitle">THE TERMINOLOGY…</h2>
+                  <h1 className="section-title">{name}</h1>
+                </div>
+                <p className="section-text">{description}</p>
+              </motion.div>
+            </AnimatePresence>
           </div>
         </div>
       </div>
